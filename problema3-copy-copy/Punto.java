623fce2ce4494034c398f30e229eb407b6b9d0c2
JosuePerezValenzuela <82065156+JosuePerezValenzuela@users.noreply.github.com>
2021-04-20 22:47:38 -0400

    Add files via upload

---
+++ problema3-copy-copy/Punto.java
@@ -0,0 +1,17 @@
+
+public class Punto
+{
+    double x;
+    double y;
+    public Punto(double coorX,double coorY)
+    {
+        x = coorX;
+        y = coorY;
+    }
+    public double getx(){
+       return x;
+    }
+    public double gety(){
+       return y;
+    }
+}
---
+++ problema3-copy-copy/resultado.java
@@ -0,0 +1,11 @@
+
+public class resultado
+{
+    double x1;
+    double x2;
+    public resultado(double resul1, double resul2)
+    {
+       x1 = resul1;
+       x2 = resul2;
+    }
+}
